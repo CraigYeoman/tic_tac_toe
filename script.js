e73f6b6963d94a614baf16b6281f025b5e5c29e8
CraigYeoman <yeomancraig@gmail.com>
2022-05-07 08:20:43 -0500

    Working on revealing module pattern

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const ticTacToe = {
-    playerArray: [],
-    computerArray: [],
-    gameBoardArray: [0,1,2,3,4,5,6,7,8],
-    winingCombination: [
+const ticTacToe = (function(){
+    'use strict';
+    let playerArray = [];
+    let computerArray = [];
+    let gameBoardArray = [0,1,2,3,4,5,6,7,8];
+    let winingCombination = [
         [0,1,2], 
         [3,4,5],
         [6,7,8],
@@ -11,46 +12,41 @@ const ticTacToe = {
         [2,5,8],
         [0,4,8],
         [2,4,6]
-    ],
-    init: function() {
-        this.cacheDom();
-        this.bindEvents();
-        //this.playerTurn();
-    },
-    cacheDom: function() {
-        this.gameBoardElements = document.querySelectorAll('[data-cell]');
-        this.endGame = document.querySelector('[data-end-game]');
-        this.endGameMessage = document.querySelector('[data-end-game-message]');
-        this.endGameBackGround = document.querySelector('[data-background-end-game]');
-        this.restartButton = document.querySelector('[restart-button]');  
-    },
-    bindEvents: function() {
-        this.endGame.addEventListener('click', this.boardReset);
-        this.gameBoardElements.forEach(cell => {cell.addEventListener('click', this.playerTurn.bind(this), { once: true});})
-    },
-    playerTurn: function(e) {
+    ];
+
+    //cacheDom
+       const gameBoardElements = document.querySelectorAll('[data-cell]');
+       const endGame = document.querySelector('[data-end-game]');
+       const endGameMessage = document.querySelector('[data-end-game-message]');
+       const endGameBackGround = document.querySelector('[data-background-end-game]');
+       const restartButton = document.querySelector('[restart-button]');  
+    //bindEvents
+        endGame.addEventListener('click', boardReset);
+        gameBoardElements.forEach(cell => {cell.addEventListener('click', playerTurn, { once: true});})
+
+    function playerTurn(e) {
         e.target.textContent = "X"
         let i = Number(e.target.id);
-        this.playerArray.push(i);
-        this.game();
-    },
-    game: function() {
-        if (this.boardEvaluation(this.playerArray)) {
+        playerArray.push(i);
+        game();
+    };
+    function game() {
+        if (boardEvaluation(playerArray)) {
             console.log('Player win');
-            this.endModal('Player');
-        } else if (this.draw()) {
+            endModal('Player');
+        } else if (draw()) {
             console.log('Draw');
-            this.endModal('Draw');
-        } else if (this.computerTurn()) {
+            endModal('Draw');
+        } else if (computerTurn()) {
             console.log('Computer wins');
-            this.endModal('Computer');
+            endModal('Computer');
         }
-    },
-    computerTurn: function() {
-        let pickedCells = this.playerArray.concat(this.computerArray);
+    };
+    function computerTurn() {
+        let pickedCells = playerArray.concat(computerArray);
         let computerPossibleChoices = [0,1,2,3,4,5,6,7,8];
         pickedCells.sort(function(a, b){return a - b});
-        this.gameBoardArray.forEach(option => {
+        gameBoardArray.forEach(option => {
             let i = 0;    
             while (i < pickedCells.length) {
                 if (pickedCells[i] === option) {
@@ -64,41 +60,44 @@ const ticTacToe = {
             return computerPossibleChoices; 
         })
         let computerChoice = computerPossibleChoices[Math.floor(Math.random() * computerPossibleChoices.length)];
-        this.computerArray.push(computerChoice);
+        computerArray.push(computerChoice);
         let gameBoardQuery = document.getElementById(computerChoice);
         gameBoardQuery.textContent = "O";
-        gameBoardQuery.removeEventListener('click', this.playerTurn, true);
-        return this.boardEvaluation(this.computerArray)
-    },
-    draw: function () {
-        let pickedCells = this.playerArray.concat(this.computerArray);
+        gameBoardQuery.removeEventListener('click', playerTurn);
+        return boardEvaluation(computerArray)
+    };
+    function draw() {
+        let pickedCells = playerArray.concat(computerArray);
         pickedCells.sort(function(a, b){return a - b});
-        pickedCellsString = pickedCells.toString();
-        gameBoardArrayString = this.gameBoardArray.toString();
+        let pickedCellsString = pickedCells.toString();
+        let gameBoardArrayString = gameBoardArray.toString();
         return pickedCellsString == gameBoardArrayString;     
-    },
-    boardEvaluation: function (array) {
-        return this.winingCombination.some(combination => {
+    };
+    function boardEvaluation(array) {
+        return winingCombination.some(combination => {
             return combination.every(element => array.includes(element))       
             })
-    },
-    endModal: function (info) {
-        this.endGame.classList.add('activate');
-        this.endGameBackGround.classList.add('activate');
+    };
+    function endModal(info) {
+        endGame.classList.add('activate');
+        endGameBackGround.classList.add('activate');
         if (info ===  'Draw') {
-            this.endGameMessage.textContent = info
+            endGameMessage.textContent = info
         } else {
-            this.endGameMessage.textContent = info+' '+'Wins';
+            endGameMessage.textContent = info+' '+'Wins';
         }
-    },
-    boardReset: function () {
-        ticTacToe.playerArray = [];
-        ticTacToe.computerArray = [];
-        ticTacToe.gameBoardElements.forEach(cell => cell.textContent = '');
-        ticTacToe.endGame.classList.remove('activate');
-        ticTacToe.endGameBackGround.classList.remove('activate');
-        ticTacToe.bindEvents();
-    },
-};
-
-ticTacToe.init();
+    };
+    function boardReset() {
+        playerArray = [];
+        computerArray = [];
+        gameBoardElements.forEach(cell => cell.textContent = '');
+        endGame.classList.remove('activate');
+        endGameBackGround.classList.remove('activate');
+        gameBoardElements.forEach(cell => {
+            cell.addEventListener('click', playerTurn, { once: true});
+        })
+    };
+    return {
+        playerTurn: playerTurn,
+    };
+})();
